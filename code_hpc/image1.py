235451c79d74bd5d40ec45d11bd79e64829728d2
Malda_Zhang <zixizhang2002@outlook.com>
2025-04-26 17:34:27 +0100

    fix image processing

--- code_hpc/image1.py
+++ code_hpc/image1.py
@@ -32,11 +32,6 @@ def get_z_coords(sitk_img):
     sz, sp, org, dir3 = sitk_img.GetSize(), sitk_img.GetSpacing(), sitk_img.GetOrigin(), sitk_img.GetDirection()
     return [org[2] + i * sp[2] * dir3[8] for i in range(sz[2])]
 
-# matching z
-def align_segmentation_by_z(seg_img, seg_z, ct_z):
-    zset = set(round(z, 1) for z in ct_z)
-    return sitk.GetArrayFromImage(seg_img)[[i for i, z in enumerate(seg_z) if round(z, 1) in zset]]
-
 # resample（1mm*3）
 def resample_to_spacing(image_sitk, new_spacing=(1.0, 1.0, 1.0), interpolator=sitk.sitkLinear):
     original_spacing = image_sitk.GetSpacing()
@@ -53,7 +48,100 @@ def resample_to_spacing(image_sitk, new_spacing=(1.0, 1.0, 1.0), interpolator=si
     resampler.SetInterpolator(interpolator)
     return resampler.Execute(image_sitk)
 
-# normalise to [0,1]
+# main
+def load_aligned_ct_and_mask(patient_folder, target_spacing=(1.5, 1.5, 1.5)):
+    # Find asegmentation
+    seg_path = next((os.path.join(root, f) for root, _, files in os.walk(patient_folder)
+                     for f in files if "segmentation" in root.lower() and f.endswith(".dcm")), None)
+    if not seg_path:
+        return None, None, None
+
+    # Matched CT
+    uid = get_series_uid_from_seg(seg_path)
+    ct_folder = find_ct_folder_by_uid(patient_folder, uid)
+    if not ct_folder:
+        return None, None, None
+
+    # reading
+    ct_img = read_dicom_series(ct_folder)
+    seg_img = sitk.ReadImage(seg_path)
+
+    # resample
+    ct_img = resample_to_spacing(ct_img, new_spacing=target_spacing)
+
+    # resample segmentation
+    resampler = sitk.ResampleImageFilter()
+    resampler.SetReferenceImage(ct_img)
+    resampler.SetInterpolator(sitk.sitkNearestNeighbor)
+    seg_img = resampler.Execute(seg_img)
+
+    # get mask
+    mask = sitk.GetArrayFromImage(seg_img)
+    mask = (mask > 0)
+
+    # get CT image
+    image = sitk.GetArrayFromImage(ct_img)
+
+    return image, mask, target_spacing
+
+def find_segmentation_dcm(patient_folder):
+    """
+    Recursively search under patient_folder to find the first subfolder containing the Segmentation keyword,
+    And there is DICOM file inside, return this dcm file path.
+    """
+    for root, dirs, files in os.walk(patient_folder):
+        if "segmentation" in root.lower():
+            dcm_files = [os.path.join(root, f) for f in files if f.lower().endswith(".dcm")]
+            if dcm_files:
+                return dcm_files[0]
+    return None
+
+
+base_dir = os.path.join(DATA_DIR, "dataset1")
+
+for patient_id in sorted(os.listdir(base_dir)):
+    if not patient_id.startswith("LUNG1-"):
+        continue  # Only process the patient folders
+
+    patient_path = os.path.join(base_dir, patient_id)
+    if not os.path.isdir(patient_path):
+        continue
+
+    # Automatically find Segmentation DICOM
+    seg_dcm_path = find_segmentation_dcm(patient_path)
+    if seg_dcm_path is None:
+        print(f"{patient_id} skipped: No segmentation found")
+        continue
+
+    # load continued only when Segmentation was found
+    image, mask, spacing = load_aligned_ct_and_mask(patient_path)
+    if image is None or mask is None:
+        print(f"{patient_id} skipped: failed to load image/mask")
+        continue
+
+    # Standardized mask
+    mask = (mask > 0)
+
+    if np.sum(mask) < 100:
+        print(f"{patient_id} skipped: ROI too small ({np.sum(mask)} voxels)")
+        continue
+
+    roi = image * mask
+    print(f"{patient_id} ROI volume shape: {roi.shape}")
+
+
+import os
+import numpy as np
+import pandas as pd
+from skimage import measure
+from scipy.spatial.distance import pdist
+import matplotlib.pyplot as plt
+
+# Data reading and preprocessing
+DATA_DIR = "/user/home/ms13525/scratch/mshds-ml-data-2025"
+base_dir = os.path.join(DATA_DIR, "dataset1")
+
+# Normalize gray scale based on ROI
 def normalize_gray_roi_adaptive(image_np, mask_np, min_percentile=1, max_percentile=99):
     roi = image_np[mask_np > 0]
     if roi.size == 0:
@@ -62,83 +150,30 @@ def normalize_gray_roi_adaptive(image_np, mask_np, min_percentile=1, max_percent
     image_clipped = np.clip(image_np, vmin, vmax)
     return (image_clipped - vmin) / (vmax - vmin + 1e-6)
 
-# main
-def load_aligned_ct_and_mask(patient_folder, target_spacing=(1.5, 1.5, 1.5)):
-    seg_path = next((os.path.join(root, f) for root, _, files in os.walk(patient_folder)
-                     for f in files if "segmentation" in root.lower() and f.endswith(".dcm")), None)
-    if not seg_path: return None, None, None
-
-    uid = get_series_uid_from_seg(seg_path)
-    ct_folder = find_ct_folder_by_uid(patient_folder, uid)
-    if not ct_folder: return None, None, None
-
-    ct_img = read_dicom_series(ct_folder)
-    seg_img = sitk.ReadImage(seg_path)
-
-    # Resample to same spacing (1mm)
-    ct_img = resample_to_spacing(ct_img, new_spacing=target_spacing)
-    
-    # resample segmentation
-    resampler = sitk.ResampleImageFilter()
-    resampler.SetReferenceImage(ct_img)
-    resampler.SetInterpolator(sitk.sitkNearestNeighbor)
-    seg_img = resampler.Execute(seg_img)
-
-    # z coordinate alignment
-    ct_z = get_z_coords(ct_img)
-    seg_z = get_z_coords(seg_img)
-    mask = align_segmentation_by_z(seg_img, seg_z, ct_z)
-
-    image = sitk.GetArrayFromImage(ct_img)
-    image = normalize_gray_roi_adaptive(image, mask)
-
-    # output
-    return image, mask, target_spacing
-
-base_dir = os.path.join(DATA_DIR, "dataset1")
-
-for patient_id in sorted(os.listdir(base_dir)):
-    patient_path = os.path.join(base_dir, patient_id)
-    if not os.path.isdir(patient_path):
-        continue  # skip non-folders
-
-    image, mask, spacing = load_aligned_ct_and_mask(patient_path)
-
-    if image is not None:
-        roi = image * (mask > 0)
-        print(f"{patient_id} ROI volume shape: {roi.shape}")
-    else:
-        print(f"{patient_id} fail")
-
-# Extraction of tumour 3D shape features based on marching cubes algorithm
-import os
-import numpy as np
-import pandas as pd
-import skimage as ski
-from skimage import io, data, filters, measure, morphology
-import matplotlib
-import matplotlib.pyplot as plt
-import nibabel as nib
-import skimage.measure
-from scipy.spatial.distance import  pdist
-
+# get shape features
 def extract_shape_features(mask, spacing=(1.0, 1.0, 1.0)):
+    mask = mask.astype(np.float32)
+
+    if np.sum(mask) == 0:
+        return {"volume_mm3": 0, "surface_mm2": 0, "max_diameter_mm": 0, "compactness": 0}
+        
     verts, faces, _, _ = measure.marching_cubes(mask, level=0.5, spacing=spacing)
 
-    # surface area
     surface_area = measure.mesh_surface_area(verts, faces)
-
-    # volumetric
     voxel_volume = np.prod(spacing)
     volume = np.sum(mask > 0) * voxel_volume
 
-    # maximum diameter
-    if len(verts) >= 2:
-        max_diameter = pdist(verts).max()
+    if len(verts) > 2000:
+        idx = np.random.choice(len(verts), size=2000, replace=False)
+        verts_sampled = verts[idx].astype(np.float32)
+    else:
+        verts_sampled = verts.astype(np.float32)
+
+    if len(verts_sampled) >= 2:
+        max_diameter = pdist(verts_sampled).max()
     else:
         max_diameter = 0.0
 
-    # Compactness
     if volume > 0:
         compactness = (surface_area ** 3) / (volume ** 2)
     else:
@@ -151,20 +186,7 @@ def extract_shape_features(mask, spacing=(1.0, 1.0, 1.0)):
         "compactness": compactness
     }
 
-for patient_id in sorted(os.listdir(base_dir)):
-    patient_path = os.path.join(base_dir, patient_id)
-    if not os.path.isdir(patient_path):
-        continue
-
-    image, mask, spacing = load_aligned_ct_and_mask(patient_path)
-    if image is None or mask is None:
-        print(f"{patient_id} skip")
-        continue
-
-    features = extract_shape_features(mask, spacing)
-    print(f"{patient_id} features:", features)
-
-# GLCM
+# get Texture Features: contrast, correlation, dissimilarity, homogeneity
 def gray_level_cooccurrence_features(img, mask, levels=32):
     bin_img = (img * (levels - 1)).astype(np.uint8)
     glcm = _calculate_glcm2(bin_img, mask, levels)
@@ -178,7 +200,6 @@ def gray_level_cooccurrence_features(img, mask, levels=32):
     sigma_x = np.std(glcm, axis=0, keepdims=True)
     sigma_y = np.std(glcm, axis=1, keepdims=True)
 
-    # downscale protection
     sigma_x[sigma_x < 1e-3] = 1e-3
     sigma_y[sigma_y < 1e-3] = 1e-3
 
@@ -191,7 +212,7 @@ def gray_level_cooccurrence_features(img, mask, levels=32):
 
     return features
 
-# GLCM2
+# get Texture Features: GLCM in 13 directions
 def _calculate_glcm2(img, mask, nbins):
     out = np.zeros((nbins, nbins, 13))
     offsets = [
@@ -206,13 +227,16 @@ def _calculate_glcm2(img, mask, nbins):
     bins = np.arange(0, nbins + 1)
 
     for i, offset in enumerate(offsets):
-        matrix1 = np.ravel(matrix[max(offset[0], 0):s[0]+min(offset[0], 0),
-                                  max(offset[1], 0):s[1]+min(offset[1], 0),
-                                  max(offset[2], 0):s[2]+min(offset[2], 0)])
-
-        matrix2 = np.ravel(matrix[max(-offset[0], 0):s[0]+min(-offset[0], 0),
-                                  max(-offset[1], 0):s[1]+min(-offset[1], 0),
-                                  max(-offset[2], 0):s[2]+min(-offset[2], 0)])
+        matrix1 = np.ravel(matrix[
+            max(offset[0], 0):s[0]+min(offset[0], 0),
+            max(offset[1], 0):s[1]+min(offset[1], 0),
+            max(offset[2], 0):s[2]+min(offset[2], 0)
+        ])
+        matrix2 = np.ravel(matrix[
+            max(-offset[0], 0):s[0]+min(-offset[0], 0),
+            max(-offset[1], 0):s[1]+min(-offset[1], 0),
+            max(-offset[2], 0):s[2]+min(-offset[2], 0)
+        ])
 
         try:
             out[:, :, i] = np.histogram2d(matrix1, matrix2, bins=bins)[0]
@@ -222,36 +246,7 @@ def _calculate_glcm2(img, mask, nbins):
 
     return out
 
-import os
-import pandas as pd
-
-base_dir = os.path.join(DATA_DIR, "dataset1")
-all_features = []
-
-for patient_id in sorted(os.listdir(base_dir)):
-    if not patient_id.startswith("LUNG1-"):
-        continue
-
-    patient_path = os.path.join(base_dir, patient_id)
-    if not os.path.isdir(patient_path):
-        continue
-
-    image, mask, spacing = load_aligned_ct_and_mask(patient_path)
-
-    if image is not None and mask is not None:
-        image = normalize_gray_roi_adaptive(image, mask)
-        glcm = gray_level_cooccurrence_features(image, mask)
-
-        glcm["patient_id"] = patient_id
-        all_features.append(glcm)
-    else:
-        print(f"{patient_id} skip")
-
-import os
-import pandas as pd
-import numpy as np
-
-base_dir = os.path.join(DATA_DIR, "dataset1")
+# Main processing
 all_features = []
 
 for patient_id in sorted(os.listdir(base_dir)):
@@ -264,10 +259,19 @@ for patient_id in sorted(os.listdir(base_dir)):
 
     image, mask, spacing = load_aligned_ct_and_mask(patient_path)
     if image is None or mask is None:
-        print(f"{patient_id} skip")
+        print(f"{patient_id} skip (no image or mask)")
+        continue
+
+    mask = (mask > 0)  # Only the tumor area was retained
+
+    # Skip the minimal ROI
+    if np.sum(mask) < 100:
+        print(f"{patient_id} skip: ROI too small ({np.sum(mask)} voxels)")
         continue
 
     image = normalize_gray_roi_adaptive(image, mask)
+
+    # get segmentation
     shape_feats = extract_shape_features(mask, spacing)
     glcm_feats = gray_level_cooccurrence_features(image, mask)
 
@@ -276,9 +280,9 @@ for patient_id in sorted(os.listdir(base_dir)):
     features.update(glcm_feats)
     all_features.append(features)
 
-# Save the result as a csv file
-output_path = "../result/combined_features.csv"
+# save result
+output_path = "../result/features1.csv"
 os.makedirs(os.path.dirname(output_path), exist_ok=True)
 df = pd.DataFrame(all_features)
-df.to_csv("../result/features1.csv", index=False)
-print(f"Done,{len(df)} in total")
+df.to_csv(output_path, index=False)
+print(f"Done, {len(df)} patients processed.")
